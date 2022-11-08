38bf7cebead3089b76ea88813beb1d88852c665b
ravifriend2u <ravi.friend2u@gmail.com>
2022-11-08 19:09:13 +0530

    test 8

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "<h1 style='text-align: center;'> Ravi's Capstone Project!</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) 
