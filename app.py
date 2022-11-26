99a133b5d6dd6036f0bab048d9dc2a235dd809bc
ravifriend2u <ravi.friend2u@gmail.com>
2022-11-26 16:17:41 +0530

    V2 changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1 style='text-align: center;'> Ravi's Capstone Project! - Version 1 </h1>"
+    return "<h1 style='text-align: center;'> Ravi's Capstone Project! - Version 2 </h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) 
