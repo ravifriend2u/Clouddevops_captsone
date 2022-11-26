816d1671a507b08c6abb052a4a51dcf539f7bb1a
ravifriend2u <ravi.friend2u@gmail.com>
2022-11-26 15:20:44 +0530

    lint failure 1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='text-align: center;'> Ravi's Capstone Project!</h1>"
+    Master
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) 
